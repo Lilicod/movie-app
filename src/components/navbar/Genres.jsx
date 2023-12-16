543cfd5cd1f16d11d0c3df6f6a5d9b1dfafaeaa1
Lilia <liliabendjeddou9@gmail.com>
2023-12-16 02:14:48 +0100

    fix a bug

--- src/components/navbar/Genres.jsx
+++ src/components/navbar/Genres.jsx
@@ -7,10 +7,10 @@ export default function Genres() {
     <div className='  font-atkinson border p-2 shadow-md rounded-lg '>
     <ul className='max-h-[300px] py-2 first:pt-0 z-60 last:pb-0 overflow-y-auto'>
         {genres.map((genre) => (
-                <Link href={`/genres/${genre.name}`}>
+                <Link href={`/genres/${genre.name}`}  key={genre.id}>
                 <li
                     className=' py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:bg-gray-700'
-                    key={genre.id}
+                   
                 >
                     {genre.name}
                 </li>
